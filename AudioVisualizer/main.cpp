4f40344c584d2ecd3347644737257beef3ddf7d1
David-Parkerr <davidparker0040@gmail.com>
2024-10-30 20:08:57 -0400

    Refactor code to have main at bottom

--- AudioVisualizer/main.cpp
+++ AudioVisualizer/main.cpp
@@ -3,95 +3,17 @@
 
 #pragma comment (lib, "d3d11.lib")
 
-void InitD3D(HWND hwnd);
-
 IDXGISwapChain* swapChain;
 ID3D11Device* device;
 ID3D11DeviceContext* context;
 D3D_FEATURE_LEVEL selectedFeatureLevel;
 
-void MessageLoop();
-void CleanUp();
-
-// Event handling
-LRESULT CALLBACK WndProc (
-	HWND hWnd,       // The window's handle
-	UINT message,    // The message's code
-	WPARAM wParam,   // Extra message info
-	LPARAM lParam    // Exra message info
-)
+// Must release directx objects that start with i should be released
+void CleanUp()
 {
-	switch (message)
-	{
-		case WM_DESTROY: PostQuitMessage(0); break;
-		default: return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
-}
-
-// Replacement for main
-int WINAPI wWinMain (
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPWSTR lpCmdLine,
-	int nCmdShow
-)
-{
-	// Register class
-	WNDCLASSEX wcex;
-	wcex.cbSize = sizeof(WNDCLASSEX);              // Size in RAM
-	wcex.style = CS_HREDRAW | CS_VREDRAW;          // Window style
-	wcex.lpfnWndProc = WndProc;                    // WndProc
-	wcex.cbClsExtra = 0;                           // These two are for allocating extra bytes
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;                    // Handle to the instance
-	wcex.hIcon = NULL;                             // Icon in the top left
-	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW); // Cursor
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = nullptr;                   // No menu
-	wcex.lpszClassName = L"MainWindow";            // Class's name
-	wcex.hIconSm = NULL;                           // No small icon
-	if (!RegisterClassEx(&wcex))
-	{
-		return 0;
-	}
-
-	// Create window
-	RECT rc = { 0, 0, 800, 600 };
-	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-	HWND hwnd = CreateWindow(
-		L"MainWindow", // Class's name
-		L"Audio Visualizer", // Window Title
-		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, // Style
-		CW_USEDEFAULT, // Window position X
-		CW_USEDEFAULT, // Window position Y
-		rc.right - rc.left, // Window Width
-		rc.bottom - rc.top, // Window Height
-		nullptr, // No parent window
-		nullptr, // No menu
-		NULL, // Handle to the instance
-		nullptr // No params passed
-	);
-
-	if (!hwnd) // Check if hwnd was created
-	{
-		return 0;
-	}
-
-	// Make sure our coms are set to 0
-	swapChain = 0;
-	device = 0;
-	context = 0;
-
-	InitD3D(hwnd);
-
-	// Display the window
-	ShowWindow(hwnd, nCmdShow);
-
-	// Main Window message loop
-	MessageLoop();
-
-	return 0;
+	if (swapChain) swapChain->Release();
+	if (device) device->Release();
+	if (context) context->Release();
 }
 
 void MessageLoop()
@@ -112,14 +34,6 @@ void MessageLoop()
 	}
 
 	CleanUp();
-}
-
-// Must release directx objects that start with i should be released
-void CleanUp()
-{
-	if (swapChain) swapChain->Release();
-	if (device) device->Release();
-	if (context) context->Release();
 }
 
 void InitD3D(HWND hwnd)
@@ -188,4 +102,86 @@ void InitD3D(HWND hwnd)
 		&context
 	);
 
+}
+
+// Event handling
+LRESULT CALLBACK WndProc(
+	HWND hWnd,       // The window's handle
+	UINT message,    // The message's code
+	WPARAM wParam,   // Extra message info
+	LPARAM lParam    // Exra message info
+)
+{
+	switch (message)
+	{
+	case WM_DESTROY: PostQuitMessage(0); break;
+	default: return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Replacement for main
+int WINAPI wWinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPWSTR lpCmdLine,
+	int nCmdShow
+)
+{
+	// Register class
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);              // Size in RAM
+	wcex.style = CS_HREDRAW | CS_VREDRAW;          // Window style
+	wcex.lpfnWndProc = WndProc;                    // WndProc
+	wcex.cbClsExtra = 0;                           // These two are for allocating extra bytes
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;                    // Handle to the instance
+	wcex.hIcon = NULL;                             // Icon in the top left
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW); // Cursor
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = nullptr;                   // No menu
+	wcex.lpszClassName = L"MainWindow";            // Class's name
+	wcex.hIconSm = NULL;                           // No small icon
+	if (!RegisterClassEx(&wcex))
+	{
+		DWORD dwError = GetLastError();
+		if (dwError != ERROR_CLASS_ALREADY_EXISTS)
+		{
+			return HRESULT_FROM_WIN32(dwError);
+		}
+	}
+
+	// Create window
+	RECT rc = { 0, 0, 800, 600 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+	HWND hwnd = CreateWindow(
+		L"MainWindow", // Class's name
+		L"Audio Visualizer", // Window Title
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, // Style
+		CW_USEDEFAULT, // Window position X
+		CW_USEDEFAULT, // Window position Y
+		rc.right - rc.left, // Window Width
+		rc.bottom - rc.top, // Window Height
+		nullptr, // No parent window
+		nullptr, // No menu
+		NULL, // Handle to the instance
+		nullptr // No params passed
+	);
+
+	if (hwnd == NULL)
+	{
+		DWORD dwError = GetLastError();
+		return HRESULT_FROM_WIN32(dwError);
+	}
+
+	// Make sure our coms are set to 0
+	swapChain = 0;
+	device = 0;
+	context = 0;
+
+	InitD3D(hwnd);
+	ShowWindow(hwnd, nCmdShow);
+	MessageLoop();
+
+	return 0;
 }
