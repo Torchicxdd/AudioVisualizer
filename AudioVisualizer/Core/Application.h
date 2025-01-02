a2656d5a4aae8fb573107a65694019a97b2e8d8e
David-Parkerr <davidparker0040@gmail.com>
2025-01-02 02:47:49 -0500

    Create application header

--- AudioVisualizer/Core/Application.h
+++ AudioVisualizer/Core/Application.h
@@ -1,1 +1,32 @@
 #pragma once
+#ifndef _APPLICATION_H_
+#define _APPLIACATION_H_
+
+// Includes
+#include <Windows.h>
+
+// Globals
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.3f;
+
+class Application
+{
+public:
+	Application();
+	Application(const Application&);
+	~Application();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	void Frame();
+
+private:
+	bool Render();
+
+private:
+
+};
+
+#endif // !_APPLICATION_H_
